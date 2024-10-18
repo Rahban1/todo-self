cca7107d5b5977827e5e008f5270a764f8daf714
rahban <rahban.ghani2001@gmail.com>
2024-10-19 01:25:03 +0530

    added function to submit on enter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors"
+import axios from "axios"
 
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("hello from backend")
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,13 +11,19 @@ function App() {
     setTodos([...todos,value])
   }
 
+  function handleClickEnter(e : React.KeyboardEvent<HTMLInputElement>){
+    if(e.key === 'Enter'){
+      handleClick();
+    }
+  }
+
   return (
   <div>
     <Navbar/>
     <div className="box-border h-screen w-full flex justify-center items-center flex-col gap-5">
       <div className=" border-gray-100 border-4 px-10 py-20 bg-gray-300 rounded-lg">
         <div className="font-bold text-5xl border text-center mb-8 py-2">Todo</div>
-        <input id="input" className="m-3 rounded-full focus:border-2 px-3 py-3 text-xl" type="text" placeholder="Enter Todo"/>
+        <input id="input" onKeyDown={handleClickEnter} className="m-3 rounded-full focus:border-2 px-3 py-3 text-xl" type="text" placeholder="Enter Todo"/>
         <div className="flex justify-center items-center">
           <button className="border-2 mx-auto p-3 rounded-full bg-green-400 hover:bg-green-500" onClick={handleClick}>Add todo</button>
         </div>
